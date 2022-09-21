b5e8a379d8379415bfcbdb6d7d61faa1b0b8d800
Nicolas Mosquera <nicolas.mosquera@gradiweb.com>
2022-09-21 12:06:33 -0500

    Clean up and initialize

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello this is the title</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
