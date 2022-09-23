a2fe7af8b0cd333f34d2ecfde949d62d1b84e16c
Nicolas Mosquera <nicolas.mosquera@gradiweb.com>
2022-09-23 16:23:20 -0500

    Modify Post component to get name

--- src/components/posts/index.jsx
+++ src/components/posts/index.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom'
+import { getOriginalUsers, initLoadingUsers } from '../../redux/actions/users';
+import Error from '../common/Error';
+import Loader from '../common/Loader';
 
 const Posts = () => {
   const { key } = useParams();
+  const dispatch = useDispatch();
+  const { 
+    users ,
+    loading,
+    error
+  } = useSelector(store => store.usersReducer);
+
+  useEffect(() => {
+    if(users.length) return;
+
+    dispatch(initLoadingUsers());
+    dispatch(getOriginalUsers());
+  }, [])
+  
+  const getUserName = () => {
+    const user = users.find(({id}) => id === parseInt(key))
+
+    return `Posts from ${user.name}`;
+  }
 
   return (
-    <div>{ key }</div>
+    <section>
+      {
+        loading
+        ? <Loader />
+        : error
+        ? <Error error={error} />
+        : <h1>{users.length && getUserName()}</h1>
+      }
+      { key }
+    </section>
   )
 }
 
