14aab2da47ab08136e3392ecc166283e1c096298
Nicolas Mosquera <nicolas.mosquera@gradiweb.com>
2022-10-13 15:38:31 -0500

    Add new component for Comment and styles

---
+++ src/components/comment/index.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react"
+import useClickOutside from "../../hooks/useClickOutside";
+import "../../styles/comment.css";
+
+const Comment = ({ callback }) => {
+  const [transition, setTransition] = useState('');
+  const [comments, setComments] = useState([1,2,3,4,5]);
+  const closeNode = useClickOutside(() => callback(true))
+
+  useEffect(() => {
+    setTransition('show');
+    return () => setTransition('')
+  }, [])
+  
+  return (
+    <div className={`fade ${transition}`}>
+      <section className='comment' ref={closeNode}>
+        {comments.map(comment => (
+          <p>This is a comment</p>
+        ))}
+      </section>
+    </div>
+  )
+}
+
+export default Comment
