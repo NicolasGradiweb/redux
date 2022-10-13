27381196ae9741ae8a469aafc0c7d27deb2f37d8
Nicolas Mosquera <nicolas.mosquera@gradiweb.com>
2022-10-13 10:37:12 -0500

    Add text to error component

--- src/components/common/Error.jsx
+++ src/components/common/Error.jsx
@@ -1,9 +1,9 @@
 import '../../styles/error.css'
 
-const Error = ({error}) => {
+const Error = ({error, text}) => {
   return (
     <section className="error">
-      <h2>Oops! there was en error :(</h2>
+      <h2>{`Oops! there was en error in ${text} :(`}</h2>
       <p>{ error }</p>
       <p>Try reloading</p>
     </section>
